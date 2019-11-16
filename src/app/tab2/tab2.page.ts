37ed9f07bf99513ecc4e639047388623f45a6cd9
Ramiro Gonzalez <ramirogonzalez@mercedenergy.com>
2019-11-15 18:14:43 -0800

    Updated the messenger

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -3,10 +3,30 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
-  styleUrls: ['tab2.page.scss']
+  styleUrls: ['tab2.page.scss'] 
 })
 export class Tab2Page {
 
+  messages = [
+    {
+      user: 'Doctor Professor Of Medicine',
+      createdAt: 15554090856000,
+      msg: 'Hey whats up dog?'
+    },
+    {
+      user: 'max',
+      createdAt: 155540900956000,
+      msg: 'I dont feel no good?'
+    },
+    {
+      user: 'Doctor Professor Of Medicine',
+      createdAt: 15540910560000,
+      msg: 'Are you dead?'
+    }
+  ]
   constructor() {}
+  sendMessage(){
+    
+  }
 
 }
